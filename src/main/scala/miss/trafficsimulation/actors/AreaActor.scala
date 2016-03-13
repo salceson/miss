81d9db597301204375ca57a98ee6c1d7b0ffc760
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-03-13 20:08:15 +0100

    Moar work on actor

--- src/main/scala/miss/trafficsimulation/actors/AreaActor.scala
+++ src/main/scala/miss/trafficsimulation/actors/AreaActor.scala
@@ -9,7 +9,8 @@ class AreaActor(area: Area) extends Actor {
   import AreaActor._
 
   override def receive: Receive = {
-    case TrafficInfo(roadId, turnNumber, carsPerLane) =>
+    case IncomingTrafficInfo(roadId, turnNumber, carsPerLane) =>
+    case OutgoingTrafficInfo(roadId, turnNumber, carsPerLane) =>
     case ReadyForComputation(turnNumber) =>
   }
 }
@@ -18,8 +19,20 @@ object AreaActor {
 
   def props(area: Area): Props = Props(classOf[AreaActor], area)
 
-  case class TrafficInfo(roadId: RoadId, turnNumber: Long, carsPerLane: List[Option[(Vehicle, Long)]])
+  class TrafficInfo(roadId: RoadId,
+                    timeFrameNumber: Long,
+                    carsPerLane: List[Option[(Vehicle, Long)]])
 
-  case class ReadyForComputation(turnNumber: Long)
+  case class IncomingTrafficInfo(roadId: RoadId,
+                                 timeFrameNumber: Long,
+                                 carsPerLane: List[Option[(Vehicle, Long)]])
+    extends TrafficInfo(roadId, timeFrameNumber, carsPerLane)
+
+  case class OutgoingTrafficInfo(roadId: RoadId,
+                                 timeFrameNumber: Long,
+                                 carsPerLane: List[Option[(Vehicle, Long)]])
+    extends TrafficInfo(roadId, timeFrameNumber, carsPerLane)
+
+  case class ReadyForComputation(timeFrameNumber: Long)
 
 }
