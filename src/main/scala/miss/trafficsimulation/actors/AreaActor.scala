668ab2024cd255c8883e4ca1acc3e737189419f7
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-03-13 14:29:18 +0100

    Some work on actor

--- src/main/scala/miss/trafficsimulation/actors/AreaActor.scala
+++ src/main/scala/miss/trafficsimulation/actors/AreaActor.scala
@@ -1,18 +1,25 @@
 package miss.trafficsimulation.actors
 
-import akka.actor.Actor
+import akka.actor.{Actor, Props}
+import miss.trafficsimulation.roads.{Area, RoadId}
+import miss.trafficsimulation.traffic.Vehicle
 
-class AreaActor extends Actor {
+class AreaActor(area: Area) extends Actor {
 
   import AreaActor._
 
   override def receive: Receive = {
-    case x: Test =>
+    case TrafficInfo(roadId, turnNumber, carsPerLane) =>
+    case ReadyForComputation(turnNumber) =>
   }
 }
 
 object AreaActor {
 
-  case class Test()
+  def props(area: Area): Props = Props(classOf[AreaActor], area)
+
+  case class TrafficInfo(roadId: RoadId, turnNumber: Long, carsPerLane: List[Option[(Vehicle, Long)]])
+
+  case class ReadyForComputation(turnNumber: Long)
 
 }
