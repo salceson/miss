c9a0f24b9f4cb9e25643beaad4d012b6bc204f37
Michał Janczykowski <mjanczykowski@gmail.com>
2016-05-09 15:03:09 +0200

    tests fix

--- src/test/scala/miss/trafficsimulation/roads/AreaSpecification.scala
+++ src/test/scala/miss/trafficsimulation/roads/AreaSpecification.scala
@@ -7,14 +7,14 @@ import org.specs2.mutable.Specification
 class AreaSpecification extends Specification {
   "Area" should {
     "create proper roads" in {
-      val road1 = AreaRoadDefinition(RoadId(1), NS, null, null)
-      val road2 = AreaRoadDefinition(RoadId(2), SN, null, null)
-      val road3 = AreaRoadDefinition(RoadId(3), EW, null, null)
-      val road4 = AreaRoadDefinition(RoadId(4), WE, null, null)
-      val road5 = AreaRoadDefinition(RoadId(5), NS, null, null)
-      val road6 = AreaRoadDefinition(RoadId(6), SN, null, null)
-      val road7 = AreaRoadDefinition(RoadId(7), EW, null, null)
-      val road8 = AreaRoadDefinition(RoadId(8), WE, null, null)
+      val road1 = AreaRoadDefinition(RoadId(1), NS, null)
+      val road2 = AreaRoadDefinition(RoadId(2), SN, null)
+      val road3 = AreaRoadDefinition(RoadId(3), EW, null)
+      val road4 = AreaRoadDefinition(RoadId(4), WE, null)
+      val road5 = AreaRoadDefinition(RoadId(5), NS, null)
+      val road6 = AreaRoadDefinition(RoadId(6), SN, null)
+      val road7 = AreaRoadDefinition(RoadId(7), EW, null)
+      val road8 = AreaRoadDefinition(RoadId(8), WE, null)
 
       val area = new Area(List(road1, road2, road5, road6),
         List(road3, road4, road7, road8), ConfigFactory.load("road_creation.conf"))
--- src/test/scala/miss/trafficsimulation/roads/RoadSegmentSpecification.scala
+++ src/test/scala/miss/trafficsimulation/roads/RoadSegmentSpecification.scala
@@ -12,13 +12,13 @@ class RoadSegmentSpecification extends Specification {
   "RoadSegment" should {
     "iterate through cars in proper way" in {
       val roadSegment = new RoadSegment(RoadId(1), 3, 7, None, null, NS, 5, 1)
-      roadSegment.lanes(0).cells(6).vehicle = Some(Car(VehicleId("1"), 2, 1, Yellow))
-      roadSegment.lanes(1).cells(5).vehicle = Some(Car(VehicleId("2"), 2, 1, Magenta))
-      roadSegment.lanes(2).cells(5).vehicle = Some(Car(VehicleId("3"), 2, 1, Cyan))
-      roadSegment.lanes(0).cells(3).vehicle = Some(Car(VehicleId("4"), 2, 1, Red))
-      roadSegment.lanes(2).cells(3).vehicle = Some(Car(VehicleId("5"), 2, 1, Green))
-      roadSegment.lanes(1).cells(2).vehicle = Some(Car(VehicleId("6"), 2, 1, Blue))
-      roadSegment.lanes(2).cells(0).vehicle = Some(Car(VehicleId("7"), 2, 1, Black))
+      roadSegment.lanes(0).cells(6).vehicle = Some(Car(VehicleId("1"), 2, 1, Yellow, 0))
+      roadSegment.lanes(1).cells(5).vehicle = Some(Car(VehicleId("2"), 2, 1, Magenta, 0))
+      roadSegment.lanes(2).cells(5).vehicle = Some(Car(VehicleId("3"), 2, 1, Cyan, 0))
+      roadSegment.lanes(0).cells(3).vehicle = Some(Car(VehicleId("4"), 2, 1, Red, 0))
+      roadSegment.lanes(2).cells(3).vehicle = Some(Car(VehicleId("5"), 2, 1, Green, 0))
+      roadSegment.lanes(1).cells(2).vehicle = Some(Car(VehicleId("6"), 2, 1, Blue, 0))
+      roadSegment.lanes(2).cells(0).vehicle = Some(Car(VehicleId("7"), 2, 1, Black, 0))
 
       val actual = roadSegment.vehicleIterator().toList.map(
         (vac: VehicleAndCoordinates) => vac.vehicle.color)
