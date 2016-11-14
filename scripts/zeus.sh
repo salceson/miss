cb1806b342126e0127273211fa3df68ff152f2a0
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-11-15 00:12:56 +0100

    Final script

--- scripts/zeus.sh
+++ scripts/zeus.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
-#PBS -q q_short
-#PBS -l nodes=3:ppn=4
-#PBS -l walltime=00:02:00
+#PBS -q l_short
+#PBS -l nodes=3:ppn=12
+#PBS -l walltime=00:10:00
 
-module load plgrid/tools/java8/oracle/1.8.0
+module add plgrid/tools/java8/oracle/1.8.0
 
 SUPERVISOR_HOSTNAME=`hostname`
 REMOTE_HOSTS=`cat ${PBS_NODEFILE} | uniq | grep -v ${SUPERVISOR_HOSTNAME}`
 
-java -Dakka.remote.netty.tcp.hostname=${SUPERVISOR_HOSTNAME} \
--Dtrafficsimulation.time.seconds=20 \
--Dtrafficsimulation.city.cols=2 \
--Dtrafficsimulation.city.rows=2 \
--Dworker.nodes=2 \
--Dworker.cores=4 \
--jar supervisor.jar &
+${JAVA_HOME}/bin/java -Dakka.remote.netty.tcp.hostname=${SUPERVISOR_HOSTNAME} \
+    -Dtrafficsimulation.time.seconds=20 \
+    -Dtrafficsimulation.city.cols=4 \
+    -Dtrafficsimulation.city.rows=4 \
+    -Dworker.nodes=2 \
+    -Dworker.cores=12 \
+    -Dakka.remote.log-remote-lifecycle-events=off \
+    -jar ${PBS_O_WORKDIR}/supervisor.jar &
+SUPERVISOR_PID=$!
+
+sleep 15
 
 for WORKER_HOST in ${REMOTE_HOSTS}
 do
-    pbsdsh -h ${WORKER_HOST} java -Dsupervisor.hostname=${SUPERVISOR_HOSTNAME} \
-    -Dakka.remote.netty.tcp.hostname=${WORKER_HOST} \
-    -jar worker.jar
+    pbsdsh -c 1 -h ${WORKER_HOST} ${JAVA_HOME}/bin/java -Dsupervisor.hostname=${SUPERVISOR_HOSTNAME} \
+        -Dakka.remote.netty.tcp.hostname=${WORKER_HOST} \
+        -Dakka.remote.log-remote-lifecycle-events=off \
+        -jar ${PBS_O_WORKDIR}/worker.jar &
 done
 
-fg
+wait ${SUPERVISOR_PID}
 exit 0
