69f9b403b90fc66ae11595ac894bb8fad406f1b1
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-11-15 23:59:52 +0100

    Final script

--- scripts/zeus.sh
+++ scripts/zeus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q l_short
-#PBS -l nodes=3:ppn=12
+#PBS -l nodes=25:ppn=12
 #PBS -l walltime=00:10:00
 
 module add plgrid/tools/java8/oracle/1.8.0
@@ -8,12 +8,12 @@ module add plgrid/tools/java8/oracle/1.8.0
 SUPERVISOR_HOSTNAME=`hostname`
 REMOTE_HOSTS=`cat ${PBS_NODEFILE} | uniq | grep -v ${SUPERVISOR_HOSTNAME}`
 
-${JAVA_HOME}/bin/java -Dakka.remote.netty.tcp.hostname=${SUPERVISOR_HOSTNAME} \
+$JAVA_HOME/bin/java -Dakka.remote.netty.tcp.hostname=${SUPERVISOR_HOSTNAME} \
     -Dtrafficsimulation.time.seconds=20 \
     -Dtrafficsimulation.city.cols=4 \
     -Dtrafficsimulation.city.rows=4 \
-    -Dworker.nodes=2 \
-    -Dworker.cores=12 \
+    -Dworker.nodes=24 \
+    -Dworker.cores=1 \
     -Dakka.remote.log-remote-lifecycle-events=off \
     -jar ${PBS_O_WORKDIR}/supervisor.jar &
 SUPERVISOR_PID=$!
