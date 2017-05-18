5cc5336aea38e3141cb8eef2e7b7039b69d9505c
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2017-05-18 22:33:42 +0200

    Added correct config

--- worker/src/main/scala/miss/worker/WorkerApp.scala
+++ worker/src/main/scala/miss/worker/WorkerApp.scala
@@ -8,7 +8,8 @@ object WorkerApp extends App {
   private def startRemoteWorkerSystem() = {
     val config = ConfigFactory.load("worker")
     val supervisorHostname = config.getString("supervisor.hostname")
-    val supervisorPath = s"akka.tcp://TrafficSimulation@$supervisorHostname:6666/user/Supervisor"
+    val supervisorPort = config.getString("supervisor.port")
+    val supervisorPath = s"akka.tcp://TrafficSimulation@$supervisorHostname:$supervisorPort/user/Supervisor"
 
     val retryIntervalSeconds = config.getLong("supervisor.association.retry.interval.seconds")
 
